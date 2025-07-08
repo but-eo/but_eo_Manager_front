3f6af3c6e0a5ff22103c664291bccdd8eb96a84f
SangHyun Oh <132821751+ohsang14@users.noreply.github.com>
2025-07-08 10:22:24 +0900

    기본 내용

---
+++ src/main/java/manager/but_eo_manager_front/ButEoManagerFrontApplication.java
@@ -0,0 +1,13 @@
+package manager.but_eo_manager_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButEoManagerFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButEoManagerFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/manager/but_eo_manager_front/ButEoManagerFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package manager.but_eo_manager_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButEoManagerFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
